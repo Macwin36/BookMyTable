9c09cacc8a82aad862df70869cfdf30de7a638e4
kishan patel <macwin36@gmail.com>
2020-08-02 13:18:44 +0530

    complete  both UI with button click event and add some validation for edittext field at 2-8-2020 1.18 pm

--- app/src/main/java/com/example/bookmytable/MainActivity.java
+++ app/src/main/java/com/example/bookmytable/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.bookmytable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText emailId,password;
-    Button btnLogin;
+    private EditText emailId,password;
+    private Button btnLogin;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +33,61 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,SignUpActivity.class);
+                /*Intent i=new Intent(MainActivity.this,SignUpActivity.class);
                 startActivity(i);
                 Toast.makeText(MainActivity.this, "btnLogin clicked", Toast.LENGTH_SHORT).show();
+*/
 
+                CreateAccount();
             }
         });
 
+    }
 
+    private void CreateAccount()
+    {
+        String email=emailId.getText().toString();
+        String pass=password.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            emailId.setError("enter valid email-id");
+            emailId.requestFocus();
+        }
+
+        if(TextUtils.isEmpty(pass) || isPasswordValid(pass) || pass.length()<8 )
+        {
+            password.setError("enter valid password");
+            password.requestFocus();
+        }
+
+        else
+        {
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            ValidateAccount(email, pass);
+        }
+    }
+
+    private void ValidateAccount(String email,String pass)
+    {
+
+    }
+
+    public static boolean isPasswordValid(String password)
+    {
+        String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!]).{8,40})";
+
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        if(password == null)
+        {
+            return false;
+        }
+        return matcher.matches();
 
     }
 }
