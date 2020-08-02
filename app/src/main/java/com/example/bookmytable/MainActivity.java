c9b2a09bf134cab1708abe6d3e8e79f951431a74
kishan patel <macwin36@gmail.com>
2020-08-02 12:59:03 +0530

    complete  both UI with button click event at 2-8-2020 12.58 pm

--- app/src/main/java/com/example/bookmytable/MainActivity.java
+++ app/src/main/java/com/example/bookmytable/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.bookmytable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText emailId,password;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailId=findViewById(R.id.login_emailid);
+        password=findViewById(R.id.login_password);
+        btnLogin=findViewById(R.id.loginBtn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(i);
+                Toast.makeText(MainActivity.this, "btnLogin clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
     }
 }
